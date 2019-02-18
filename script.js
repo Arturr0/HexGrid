00dca6e1e3b4e0d6a011e2996b7190224fcc6909
Arturr0 <41492101+Arturr0@users.noreply.github.com>
2019-02-19 00:59:31 +0100

    selecting hex

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
-var draw = SVG('drawing').size(500, 500)
+var width = 500, height = 500
+var draw = SVG('drawing').size(width, height)
+
+var edges = draw.rect(width, height).fill('none').stroke({width: 1, color: 'gray'})
 
 var selectedHex
 
-var Hex = Honeycomb.extendHex({ size: 30,
+var Hex = Honeycomb.extendHex({ size: 28,
     render(draw) 
     {
+        var position = this.toPoint()
+        var centerPosition = this.center().add(position)
+        var fontSize = 8
+
         var { x, y } = this.toPoint()
         var corners = this.corners()
-    
-        this.draw = draw
+
+        this.poly = draw
             .polygon(corners.map(({ x, y }) => `${x},${y}`))
             .fill('MidnightBlue ')
             .stroke({ width: 0.5, color: 'LightSlateGray ' })
             .translate(x, y)
+
+        /*this.coord = draw
+            .text(`${this.x},${this.y}`)
+            .font({size: fontSize, anchor: 'middle', leading: 1.4, fill: 'LIGHTGRAY'})
+            .translate(centerPosition.x, centerPosition.y + (1.5 * fontSize))*/
     },
 
     blink() 
     {
-        this.draw
-            .stop(true, true)
+        console.log('blink()')
+        this.poly
+            //.stop(true, true)
             .fill({opacity: 1})
             .animate(500)
             .fill({opacity: 0.75})
             .loop(null, true)
     },
+
     stopBlink()
     {
-        this.draw.finish()
+        this.poly.finish()
             .fill({opacity: 1})
     }
 })
